eb4ce31480773691e1611b06d9a43d7091f5bf42
Dhushyanth Ramasamy <git@dhushyanth.com>
2018-10-03 20:58:29 -0400

    postgres changes

--- tests/test_postgres.py
+++ tests/test_postgres.py
@@ -1,34 +1,49 @@
 import cyanodbc
 import base_case
+import datetime
 import pytest
 
-pytestmark = pytest.mark.skip("WIP")
+# pytestmark = pytest.mark.skip("WIP")
 class TestPostgres(base_case.BaseCase):
     driver = cyanodbc
-    connect_args = ("Driver={PostgreSQL ODBC Driver(UNICODE)};Database=test;Server=localhost;Port=5432;Uid=postgres;Pwd=Password12!;",)
+    connect_args = ("Driver={PostgreSQL Unicode(x64)};Database=test;Server=localhost;Port=5432;Uid=postgres;Pwd=Password12!;",)
     
     ddl1 = """ CREATE TABLE so_header (
-    so_header_id INTEGER PRIMARY KEY,
+    header_id INTEGER PRIMARY KEY,
     customer_id INTEGER,
-    
-    ship_to VARCHAR (255)
+    ship_to TEXT,
+    creation_date TIMESTAMP,
+    last_update_date TIMESTAMPTZ,
+    created_by varchar(60),
+    last_updated_by varchar(60),
+    last_update_login varchar(60)
     );
     """
 
     ddl2 = """ CREATE TABLE so_item (
-    so_item_id INTEGER PRIMARY KEY,
-    so_header_id INTEGER,
+    item_id INTEGER PRIMARY KEY,
+    header_id INTEGER,
     product_id INTEGER,
     qty INTEGER,
     net_price NUMERIC,
-    FOREIGN KEY (so_header_id) REFERENCES so_header (so_header_id)
+    FOREIGN KEY (header_id) REFERENCES so_header (header_id)
     );
     """
 
-    #insert1 = (cyanodbc.integer(1), cyanodbc.integer(1), cyanodbc.string("7 Racecourse Road\n Elms, NY"))
+    insert1 = [
+        ("header_id", 1),
+        ("customer_id", 47),
+        ("ship_to", "10 Racecourse Road\n Bangalore, India"),
+        ("creation_date", datetime.datetime.now()),
+        ("last_update_date", datetime.datetime.now(datetime.timezone.utc)),
+        ("created_by", "Major Frank"),
+        ("last_updated_by", "Rob Shaw"),
+        ("last_update_login", "ROBSHAW"),
+
+        ]
     def setUp(self):
         cnxn = self.driver.connect(
-                "Driver={PostgreSQL ODBC Driver(UNICODE)};Server=localhost;Port=5432;Uid=postgres;Pwd=Password12!;"
+                "Driver={PostgreSQL Unicode(x64)};Server=localhost;Port=5432;Uid=postgres;Pwd=Password12!;"
                 )
         cur = cnxn.cursor()
         cur.execute("""DROP DATABASE IF EXISTS test""")
@@ -40,7 +55,7 @@ class TestPostgres(base_case.BaseCase):
 
     def tearDown(self):
         cnxn = self.driver.connect(
-                "Driver={PostgreSQL ODBC Driver(UNICODE)};Server=localhost;Port=5432;Uid=postgres;Pwd=Password12!;"
+                "Driver={PostgreSQL Unicode(x64)};Server=localhost;Port=5432;Uid=postgres;Pwd=Password12!;"
                 )
         cur = cnxn.cursor()
 
